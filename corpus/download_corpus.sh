daf5972b80e344d1230d1c5b53bb55ad1d457421
Raz Besaleli <rbesaleli@gmail.com>
2022-10-10 12:33:52 -0400

    update

---
+++ corpus/download_corpus.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+REPO_URL=https://github.com/JEMHcorpus/corpora.git
+N_SUBMODULES_FOUND=$(cat .gitmodules | grep -c 'JEMHcorpus')
+
+if [ $N_SUBMODULES_FOUND == 1 ];
+then
+    echo 'JEMH already added as submodule'
+else
+    echo 'JEMH not found as submodule. Adding now...'
+    git submodule add $REPO_URL
+fi
+
+# update submodules recursively
+git submodule update --recursive
